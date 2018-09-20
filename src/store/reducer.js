2a995df9cbaaa91ef5c714470967022eb6eddab8
Masee Hussain <hussainmasee@gmail.com>
2018-09-20 09:42:50 +0100

    create a new array of results

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -30,9 +30,10 @@ const reducer = (state = initialState, action) => {
         results: state.results.concat({ id: new Date(), value: state.counter })
       };
     case "DELETE_RESULT":
-      const id = 2;
-      const newArray = [...state.results];
-      newArray.splice(id, 1);
+      // const id = 2;
+      // const newArray = [...state.results];
+      // newArray.splice(id, 1);
+
       return {
         ...state,
         results: newArray
