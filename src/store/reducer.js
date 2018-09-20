c6b45630d557861d91eb82d9b6ee65fadb2f8e20
Masee Hussain <hussainmasee@gmail.com>
2018-09-20 09:42:02 +0100

    create a case for delete result

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -29,6 +29,14 @@ const reducer = (state = initialState, action) => {
         ...state,
         results: state.results.concat({ id: new Date(), value: state.counter })
       };
+    case "DELETE_RESULT":
+      const id = 2;
+      const newArray = [...state.results];
+      newArray.splice(id, 1);
+      return {
+        ...state,
+        results: newArray
+      };
     default:
       return state;
   }
