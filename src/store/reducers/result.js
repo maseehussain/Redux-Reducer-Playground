9a32a0233c51f75945e56408757c47e00c7ea9fc
Masee Hussain <hussainmasee@gmail.com>
2018-09-24 14:43:06 +0100

    factorized the code in result.js

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -8,25 +8,17 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-      return;
-      return {
-        ...state,
+      return updateObject(state, {
         results: state.results.concat({
           id: new Date(),
           value: action.result
         })
-      };
+      });
     case actionTypes.DELETE_RESULT:
-      // const id = 2;
-      // const newArray = [...state.results];
-      // newArray.splice(id, 1);
       const updatedArray = state.results.filter(
         result => result.id !== action.resultElementId
       );
-      return {
-        ...state,
-        results: updatedArray
-      };
+      return updateObject(state, { results: updatedArray });
     default:
       return state;
   }
